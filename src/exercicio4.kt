bf75764a431755fddab25612d12c0eb8e0926fe2
kaiorodriguess010-lgtm <kaiorodriguess010@gmail.com>
2026-03-12 20:49:30 -0300

    Fix formatting of average calculation in exercicio4.kt

--- src/exercicio4.kt
+++ src/exercicio4.kt
@@ -1,7 +1,7 @@
 fun main() {
     val idades = listOf(10, 18, 20, 15, 30)
     val maiores = idades.filter { it >= 18 }
-    val media = maiores.average() // Calcula a média automaticamente
+    val media = maiores.average()
     println("A média é: $media")
 }
 
