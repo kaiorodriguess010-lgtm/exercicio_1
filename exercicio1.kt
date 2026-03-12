6f19b571de710caa4541412821e781418034f92e
kaiorodriguess010-lgtm <kaiorodriguess010@gmail.com>
2026-03-12 20:50:06 -0300

    Remove unnecessary comments from exercicio1.kt

--- exercicio1.kt
+++ exercicio1.kt
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
     for (i in 1..100) {
         if (i % 3 == 0 && i % 5 == 0) {
